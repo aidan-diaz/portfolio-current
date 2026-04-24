6df67ce08ae351cc8d63213efd2d039d2f16c6e3
Aidan Diaz <diaz.aidan.d@gmail.com>
2026-04-24 19:02:11 -0400

    initial scaffolding + config setups

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "monospace" }}>
+      <h1>portfolio-current</h1>
+      <p>Scaffold ready. Sections will be wired up in subsequent tasks.</p>
+    </main>
+  );
+}
